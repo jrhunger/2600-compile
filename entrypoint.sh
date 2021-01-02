a5e310eddeda7e04ffc9657d9951349219471639
jrhunger <1101010@gmail.com>
2021-01-02 17:00:36 -0500

    update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-/usr/local/bin/dasm *.asm -f3 -v1 -Lcart.lst -scart.sym -ocart.bin
+if [ "$DEBUG_LEVEL" != "" ]; then
+  DEBUG_OPTION="-v$DEBUG_LEVEL"
+fi
 
+/usr/local/bin/dasm *.asm -f3 $DEBUG_OPTION -Lcart.lst -scart.sym -ocart.bin
+
+if [ "$DEBUG_LEVEL" = "2" ]; then
 echo "--------------------------- list file ----------------------------"
 cat cart.lst
 echo ""
 echo ""
 echo "--------------------------- sym file -----------------------------"
 cat cart.sym
+fi
